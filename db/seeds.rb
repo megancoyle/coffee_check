ceebbfdaa1747370da7a7439f124c8d75ebe2617
Megan Coyle <meg@mcoyle.com>
2016-08-03 21:32:02 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Shop.destroy_all
+Drink.destroy_all
+
+shop_list = [
+  ["Da Coffee Shop", "Chain", "/images/shops/shop-1.jpg"],
+  ["Coffee Break", "Hipster", "/images/shops/shop-2.jpg"],
+  ["Coffee Coffee", "Local", "/images/shops/shop-3.jpg"],
+  ["The Best Coffee Ever", "Chain", "/images/shops/shop-4.jpg"],
+  ["Amazing", "Hipster", "/images/shops/shop-5.jpg"],
+  ["Break Time", "Chain", "/images/shops/shop-6.jpg"]
+]
+
+shop_list.each do |name, shop_type, image|
+  Shop.create(name: name, shop_type: shop_type, img_url: image)
+end
+
+drink_list = [
+  ["Delish Latte", "The Best", "Light", "Yum", 1,  "/images/drinks/coffee-1.jpg" ],
+  ["Coffee Time", "Plain Coffee", "Dark", "Sweet", 2, "/images/drinks/coffee-2.jpg" ],
+  ["Espresso Yo' Self", "Espresso", "Medium", "Sweet", 3, "/images/drinks/coffee-3.jpg" ],
+  ["A Lil Cup o' Coffee", "Plain Coffee", "Medium", "Meh", 4, "/images/drinks/coffee-4.jpg" ],
+  ["Coffee 4 Me", "Latte", "Light", "Eh", 5, "/images/drinks/coffee-5.jpg" ],
+  ["Coffee Time", "Plain Coffee", "Dark", "Yes", 6, "/images/drinks/coffee-6.jpg" ]
+]
+
+drink_list.each do |name, kind_of_drink, roast, flavor, shop_id, img_url|
+  Drink.create(name: name, kind_of_drink: kind_of_drink, roast: roast, flavor: flavor, shop_id: shop_id, img_url: img_url)
+end
